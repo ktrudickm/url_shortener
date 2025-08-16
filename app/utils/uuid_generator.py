972f64c61c881bcd8d2f7799ee2b3a340123d743
Kendall <ktrudickm@gmail.com>
2025-08-15 19:50:44 -0700

    first implemented base62 encoding for random shorturl

--- app/utils/uuid_generator.py
+++ app/utils/uuid_generator.py
@@ -1,8 +1,18 @@
 from uuid import uuid4
 
+def encode_base62(num: int):
+    chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    base = len(chars)
+    encoded = ""
+    while num > 0:
+        num, rem = divmod(num, base)
+        encoded = chars[rem] + encoded
+    return encoded or chars[0]
+
 # Generate a short url from a long url using uuid4
 def generate_short_url(long_url: str):
     uuid = uuid4()
-    short_url = str(uuid)[:12]
+    u_int = uuid.int
+    short_url = encode_base62(u_int)[:6]
     return short_url
 
